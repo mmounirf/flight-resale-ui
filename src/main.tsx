9d197d6b3296977622187547709417ab039e06c6
Mohamed Mounir <m.mounir.f@gmail.com>
2023-11-24 05:28:47 +0100

    fix build

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
